88a340f47c9c5fb33010267072afd295ca5b0b26
tinoper <tinoper@gmail.com>
2020-09-23 18:03:15 -0300

    starting with profile page

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatefulWidget {
+  @override
+  _UserProfileState createState() => _UserProfileState();
+}
+
+class _UserProfileState extends State<UserProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil de usuario'),
+      ),
+      //TODO implementar diseño de perfil
+      body: Container(),
+    );
+  }
+}
