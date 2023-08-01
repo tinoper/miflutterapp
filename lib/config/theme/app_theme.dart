8e815f047f278fea9788b5dcdb974ae4eb43755a
Martin Peruchena <tinoper@gmail.com>
2023-08-01 14:11:01 -0300

    Update app_theme.dart

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.deepOrange,
+        colorSchemeSeed: Colors.amber,
         textTheme: GoogleFonts.latoTextTheme(ThemeData().textTheme),
       );
 }
